d5896790071c9b1d403a8ba15494545fe52180d8
Stephan Karsten <stephan.karsten@outlook.com>
2023-05-20 10:56:16 +0200

    test: new way to add double commands?

--- bashup.sh
+++ bashup.sh
@@ -1,19 +1,28 @@
+#!/bin/bash
 echo "The bashup script"
 
-HCOUNTER=0
+# COUNTER=1
 
-__fzf_history2__() {
-    echo "test"
-}
+# __history_up__() {
+#     # echo $COUNTER
+#     ((COUNTER++))
+#     $(previous-history)
+# }
+# __history_down__() {
+#     # echo $COUNTER
+#     if ((COUNTER > 0)); then
+#         ((COUNTER--))
+#     fi
+#     $(next-history)
+# }
+# # bind -r "\e[A"
+# # bind -m emacs-standard -x '"\e[A":__fzf_history2__'
+# # bind '"\e[A":"\C-u`__fzf_history2__`\e\C-e\C-a\C-y\C-e"'
 
-if (( BASH_VERSINFO[0] < 4 )); then
-  # CTRL-R - Paste the selected command from history into the command line
-  bind -m emacs-standard '"\e[A": "\C-e \C-u\C-y\ey\C-u"$(__fzf_history2__)"\e\C-e\er"'
-  bind -m vi-command '"\e[A": "\C-z\e[A\C-z"'
-  bind -m vi-insert '"\e[A": "\C-z\e[A\C-z"'
-else
-  # CTRL-R - Paste the selected command from history into the command line
-  bind -m emacs-standard -x '"\e[A": __fzf_history2__'
-  bind -m vi-command -x '"\e[A": __fzf_history2__'
-  bind -m vi-insert -x '"\e[A": __fzf_history2__'
-fi
+# if (( BASH_VERSINFO[0] < 4 )); then
+#     echo 'Bash version lower then 4 not supported!'
+# #   bind -m emacs-standard '"\e[A": "\C-e \C-u\C-y\ey\C-u"$(__fzf_history2__)"\e\C-e\er"'
+# else
+#     bind -m emacs-standard -x '"\e[A": __history_up__'
+#     bind -m emacs-standard -x '"\e[B": __history_down__'
+# fi
